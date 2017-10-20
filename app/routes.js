63e15789ed026fae8d297369d486064893453845
muildrik <martin_uildriks@brown.edu>
2017-10-20 00:20:27 -0400

    RPIO

--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,19 @@ module.exports = function(app) {
 	var diskspace = require('diskspace');
 	var fs = require('fs');
 	var path = "public/images/";
+	var rpio = require('rpio');
+	
+	rpio.open(12, rpio.OUTPUT, rpio.LOW);
+	
+	for (var i = 0; i < 5; i++) {
+        /* On for 1 second */
+        rpio.write(12, rpio.HIGH);
+        rpio.sleep(1);
+ 
+        /* Off for half a second (500ms) */
+        rpio.write(12, rpio.LOW);
+        rpio.msleep(500);
+	}
 	
 	// Error handling
 	process.on('unhandledRejection', function(reason, p){
